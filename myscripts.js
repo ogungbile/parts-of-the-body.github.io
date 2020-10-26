eff7fb1999da88cd1a6e069d84b39ac35f364cce
unknown <oluwatobi.ogungbile@yahoo.com>
2020-10-25 20:19:39 -0700

    message for the commit

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("myAudio1");
+    var y = document.getElementById("myAudio2");
+    var m = document.getElementById("myAudio3");
+    var n = document.getElementById("myAudio4");
+
+    function playAudio1() {
+      x.play();
+    }
+    function playAudio2() {
+      y.play();
+    }
+    function playAudio3() {
+      m.play();
+    }
+    function playAudio4() {
+      n.play();
+    }
