778f8b2fb59d32bdb30d9cd79cd243e79df1de0a
unknown <oluwatobi.ogungbile@yahoo.com>
2020-11-01 00:10:45 -0700

    5 more parts added

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,13 @@ var x = document.getElementById("myAudio1");
     var y = document.getElementById("myAudio2");
     var m = document.getElementById("myAudio3");
     var n = document.getElementById("myAudio4");
-
+    var l = document.getElementById("myAudio5");
+    var er = document.getElementById("myAudio6");
+    var mt = document.getElementById("myAudio7");
+    var ns = document.getElementById("myAudio8");
+    var h = document.getElementById("myAudio9");
+    var s = document.getElementById("myAudio10");
+    var ft = document.getElementById("myAudio11");
     function playAudio1() {
       x.play();
     }
@@ -14,4 +20,25 @@ var x = document.getElementById("myAudio1");
     }
     function playAudio4() {
       n.play();
+    }
+    function playAudio5() {
+      l.play();
+    }
+    function playAudio6() {
+      er.play();
+    }
+    function playAudio7() {
+      mt.play();
+    }
+    function playAudio8() {
+      ns.play();
+    }
+    function playAudio9() {
+      h.play();
+    }
+    function playAudio10() {
+      s.play();
+    }
+    function playAudio11() {
+      ft.play();
     }
